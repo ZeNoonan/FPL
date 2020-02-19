b6f2b21710ef66fa131a4838c86b258332610ba5
ZeNoonan <darragh.noonan@gmail.com>
2020-02-19 22:08:11 +0000

    this works for caching...

--- fpl_exponential_v3.py
+++ fpl_exponential_v3.py
@@ -3,6 +3,8 @@ import numpy as np
 import streamlit as st
 from time import time
 from pulp import *
+#
+start_uncached=time()
 
 url1='https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2019-20/players_raw.csv'
 url2='https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2018-19/players_raw.csv'
@@ -19,7 +21,47 @@ def main():
     """)
     st.markdown( f"""Source Data: [2020 Player Info]({url1}), [2019 Player Info]({url2})
     """)
+    load1_uncached=time()
+    url_2020=prep_data(url1).copy()
+    url_2019=prep_data(url2).copy()
+    url_2018=prep_data(url3).copy()
+    load2_uncached = time()
+    pick_2020=pickle_data(raw1).copy()
+    pick_2019=pickle_data(raw2).copy()
+    pick_2018=pickle_data(raw3).copy()
+    load3_uncached = time()
+    players_2020 = load_data(url_2020,pick_2020)
+    players_2019 = load_data(url_2019,pick_2019)
+    players_2018 = load_data(url_2018,pick_2018)
+    load4_uncached = time()
+    players_2020=clean_format(players_2020)
+    players_2019=clean_format(players_2019)
+    load5_uncached = time()
+    # players_2018_2020 = combine_df(players_2018, players_2019, players_2020)
+    players_2018_2020 = pd.concat ([players_2018, players_2019, players_2020], axis=0,sort = True)
+    load6_uncached = time()
+    players_2018_2020=col_df(players_2018_2020)
+    load7_uncached = time()
+    st.table (players_2018_2020.head())
+    finish_uncached = time()
 
+    benchmark_uncached = (
+        f"Cached. Total: {finish_uncached - load1_uncached:.2f}s"
+        f" Load1: {load2_uncached - load1_uncached:.2f}"
+        f" Load2: {load3_uncached - load2_uncached:.2f}"
+        f" Load3: {load4_uncached - load3_uncached:.2f}"
+        f" Load4: {load5_uncached - load4_uncached:.2f}"
+        f" Load5: {load6_uncached - load5_uncached:.2f}"
+        f" Load6: {load7_uncached - load6_uncached:.2f}"
+        # f" Load8: {finish_uncached - load7_uncached:.2f}"
+        )
+    st.text(benchmark_uncached)
+
+######
+@st.cache
+def combine_df(x,y,z):   # Actually turns out that its faster not to do the function here so not using it
+    players_2018_2020=pd.concat([x,y,z], axis=0, sort=True) # this is where the timer function came in handy from pbaumgartner
+    return players_2018_2020
 
 @st.cache
 def prep_data(url):
@@ -28,19 +70,8 @@ def prep_data(url):
 @st.cache
 def pickle_data(pick_location):
     return pd.read_pickle(pick_location)
-
-start_uncached=time()
-
-url_2020=prep_data(url1).copy()
-url_2019=prep_data(url2).copy()
-url_2018=prep_data(url3).copy()
-pick_2020=pickle_data(raw1).copy()
-pick_2019=pickle_data(raw2).copy()
-pick_2018=pickle_data(raw3).copy()
-
-load1_uncached=time()
-
-
+##
+@st.cache
 def load_data(url, pick_location):
     players_raw=url
     players_raw=players_raw.rename(columns = {'id':'player_id'})
@@ -54,58 +85,46 @@ def load_data(url, pick_location):
     players=players.rename(columns = {'name':'Name'})
     return players
 
-load2_uncached = time()
+@st.cache
+def clean_format(df):
+    f=lambda x: x.rsplit('_',1)[0]    # not sure why i'm doing this I know why its cos there is numbers after name
+    df['Name']=df['Name'].apply(f)
+    return df
 
-players_2020 = load_data(url_2020,pick_2020)
-players_2019 = load_data(url_2019,pick_2019)
-players_2018 = load_data(url_2018,pick_2018)
-# # players_2017 = load_data('https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2016-17/players_raw.csv','raw_data_2017.pkl')
+@st.cache
+def col_df(df):
+    df['Game_1'] = np.where((df['minutes'] > 0.5), 1, 0)
+    df['Expo_Pts'] = df.groupby(['Name'])['week_points'].transform(lambda x: x.ewm(alpha=0.07).mean())
+    df['Clean_Pts'] = np.where(df['Game_1']==1,df['week_points'], np.NaN)
+    df = df.sort_values(by=['Name', 'year', 'week'], ascending=[True, True, True]) # THIS IS IMPORTANT!! EWM doesn't work right unless sorted
+    df['EWM_Pts'] = df['Clean_Pts'].ewm(alpha=0.07).mean()
+    df['Games_Season_Rolling'] = df.groupby (['Name', 'year'])['Game_1'].cumsum()
+    df['Games_Season_Total'] = df.groupby (['Name', 'year'])['Game_1'].transform('sum')
+    df['Games_Total_Rolling'] = df.groupby (['Name'])['Game_1'].cumsum()
+    df['Games_Total'] = df.groupby (['Name'])['Game_1'].transform('sum')
+    df['Points_Season_Rolling'] = df.groupby (['Name', 'year'])['week_points'].cumsum()
+    df['Points_Season_Total'] = df.groupby (['Name', 'year'])['week_points'].transform('sum')
+    df['Points_Total_Rolling'] = df.groupby (['Name'])['week_points'].cumsum()
+    df['Points_Total'] = df.groupby (['Name'])['week_points'].transform('sum')
+    df['PPG_Total'] = df['Points_Total'] / df['Games_Total']
+    df['PPG_Season_Rolling'] = df['Points_Season_Rolling'] / df['Games_Season_Rolling']
+    df['PPG_Total_Rolling'] = df['Points_Total_Rolling'] / df['Games_Total_Rolling']
+    df['PPG_Season_Total'] = df['Points_Season_Total'] / df['Games_Season_Total']
+    df["GK"] = (df["Position"] == 'GK').astype(float)
+    df["DF"] = (df["Position"] == 'DF').astype(float)
+    df["MD"] = (df["Position"] == 'MD').astype(float)
+    df["FW"] = (df["Position"] == 'FW').astype(float)
+    df["LIV"] = (df["team"] == 'Liverpool').astype(float)
+    df["MC"] = (df["team"] == 'Man_City').astype(float)
+    df['team'] = df['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Bournemouth', 4:'Brighton',5:'Burnley',6:'Chelsea',7:'Crystal_Palace',
+    8:'Everton',9:'Leicester',10:'Liverpool',11:'Man_City',12:'Man_Utd',13:'Newcastle',14:'Norwich',15:'Sheffield_Utd',16:'Southampton',17:'Spurs',
+    18:'Watford',19:'West_Ham',20:'Wolves'})
 
-load3_uncached = time()
+    return df
+######
 
-f=lambda x: x.rsplit('_',1)[0]
-players_2019['Name']=players_2019['Name'].apply(f)
-players_2020['Name']=players_2020['Name'].apply(f)  # not sure why i'm doing this I know why its cos there is numbers after name
 
-players_2018_2020 = pd.concat ([players_2018, players_2019, players_2020], axis=0,sort = True)
-players_2018_2020['Game_1'] = np.where((players_2018_2020['minutes'] > 0.5), 1, 0)
-players_2018_2020['Expo_Pts'] = players_2018_2020.groupby(['Name'])['week_points'].transform(lambda x: x.ewm(alpha=0.07).mean())
 
-players_2018_2020['Clean_Pts'] = np.where(players_2018_2020['Game_1']==1,players_2018_2020['week_points'], np.NaN)
-players_2018_2020 = players_2018_2020.sort_values(by=['Name', 'year', 'week'], ascending=[True, True, True]) # THIS IS IMPORTANT!! EWM doesn't work right unless sorted
-players_2018_2020['EWM_Pts'] = players_2018_2020['Clean_Pts'].ewm(alpha=0.07).mean()
 
-players_2018_2020['Games_Season_Rolling'] = players_2018_2020.groupby (['Name', 'year'])['Game_1'].cumsum()
-players_2018_2020['Games_Season_Total'] = players_2018_2020.groupby (['Name', 'year'])['Game_1'].transform('sum')
-players_2018_2020['Games_Total_Rolling'] = players_2018_2020.groupby (['Name'])['Game_1'].cumsum()
-players_2018_2020['Games_Total'] = players_2018_2020.groupby (['Name'])['Game_1'].transform('sum')
-
-players_2018_2020['Points_Season_Rolling'] = players_2018_2020.groupby (['Name', 'year'])['week_points'].cumsum()
-players_2018_2020['Points_Season_Total'] = players_2018_2020.groupby (['Name', 'year'])['week_points'].transform('sum')
-players_2018_2020['Points_Total_Rolling'] = players_2018_2020.groupby (['Name'])['week_points'].cumsum()
-players_2018_2020['Points_Total'] = players_2018_2020.groupby (['Name'])['week_points'].transform('sum')
-
-players_2018_2020['PPG_Total'] = players_2018_2020['Points_Total'] / players_2018_2020['Games_Total']
-players_2018_2020['PPG_Season_Rolling'] = players_2018_2020['Points_Season_Rolling'] / players_2018_2020['Games_Season_Rolling']
-players_2018_2020['PPG_Total_Rolling'] = players_2018_2020['Points_Total_Rolling'] / players_2018_2020['Games_Total_Rolling']
-players_2018_2020['PPG_Season_Total'] = players_2018_2020['Points_Season_Total'] / players_2018_2020['Games_Season_Total']
-
-st.table (players_2018_2020.head())
-
-load4_uncached = time()
-
-finish_uncached = time()
-
-benchmark_uncached = (
-    f"Cached. Total: {finish_uncached - load1_uncached:.2f}s"
-    f" Load1: {load2_uncached - load1_uncached:.2f}"
-    f" Load2: {load3_uncached - load2_uncached:.2f}"
-    f" Load3: {load4_uncached - load3_uncached:.2f}"
-    # f" Load5: {load5_uncached - load4_uncached:.2f}"
-    # f" Load6: {load6_uncached - load5_uncached:.2f}"
-    # f" Load7: {load7_uncached - load6_uncached:.2f}"
-    # f" Load8: {finish_uncached - load7_uncached:.2f}"
-    )
-st.text(benchmark_uncached)
 
 main()
