f3b6a46dedc3218186e4f9a8aee5f93c6fbdc845
ZeNoonan <darragh.noonan@gmail.com>
2020-02-25 22:15:09 +0000

    update

--- fpl_exponential_v3.py
+++ fpl_exponential_v3.py
@@ -13,7 +13,7 @@ raw1='raw_data_2020.pkl'
 raw2='raw_data_2019.pkl'
 raw3='raw_data_2018.pkl'
 
-
+##
 
 
 def main():
@@ -25,45 +25,34 @@ def main():
     st.markdown( f"""Source Data: [2020 Player Info]({url1}), [2019 Player Info]({url2})
     """)
     load1_uncached=time()
-    # url_2020=prep_data(url1).copy()
-    # url_2019=prep_data(url2).copy()
-    # url_2018=prep_data(url3).copy()
-    load2_uncached = time()
-    # pick_2020=pickle_data(raw1).copy()
-    # pick_2019=pickle_data(raw2).copy()
-    # pick_2018=pickle_data(raw3).copy()
-    load3_uncached = time()
-    # players_2020 = pd.merge(url_2020,pick_2020, on='player_id')
-    # players_2019 = pd.merge(url_2019,pick_2019, on='player_id')
-    # players_2018 = pd.merge(url_2018,pick_2018, on='player_id')
-    load4_uncached = time()
-    # players_2020=clean_format(players_2020)
-    # players_2019=clean_format(players_2019)
-    load5_uncached = time()
+    
     # players_2018_2020 = combine_df(players_2018, players_2019, players_2020)
     # players_2018_2020 = pd.concat ([players_2018, players_2019, players_2020], axis=0,sort = True) ##this was quicker I found than using in a function
-    # st.table (players_2018_2020.head())
-    load6_uncached = time()
-    # players_2018_2020=col_df(players_2018_2020)
-    # st.table (players_2018_2020.head())
-
-    players_2018_2020=combine_functions()
-
-    load7_uncached = time()
-    year = st.selectbox ("Select a year",(2019,2020))
+    load2_uncached = time()
+    
+    players_2018_2020=combine_functions().copy()
+    
+    data = combine_functions()
+    load3_uncached = time()
+    year = st.selectbox ("Select a year",(2020,2019))
     week = st.slider ("Select a week", 1,26)
-    min_games_played = st.slider ("Minimum number of games played", 1,150)
-    players_2018_2020=show_data(players_2018_2020, year, week, min_games_played)
-    additional_info=players_2018_2020.loc[:,['Name','week','round', 'Games_Total','Games_Total_Rolling', 'Games_Season_Total', 'Games_Season_Rolling']] 
-    # cols_to_move = ['Name','Position','team','year','week','round','Cost','week_points','EWM_Pts','Clean_Pts','PPG_Total','points_per_game','Games_Total','minutes','Game_1',
-    # 'Games_Total_Rolling', 'Games_Season_Total','Games_Season_Rolling','PPG_Total_Rolling','PPG_Season_Rolling','PPG_Total','PPG_Season_Total']
-    # cols = cols_to_move + [col for col in players_2018_2020 if col not in cols_to_move]
-    # players_2018_2020=players_2018_2020[cols]
-    load8_uncached = time()
+    min_games_played = st.slider ("Minimum number of games played from start of 2019 Season", 1,150)
+    min_current_season_games_played = st.slider("Minimum number of games played from start of current Season", 1,38)
+    players_2018_2020=show_data(players_2018_2020, year, week, min_games_played, min_current_season_games_played)
+    # st.table (players_2018_2020.sort_values(by='points_per_game', ascending=False).head(1))
+    additional_info=players_2018_2020.loc[:,['Name','week','round', 'Games_Total','Games_Total_Rolling', 'Games_Season_Total', 'Games_Season_to_Date',
+    'PPG_Season_Rolling','points_per_game','Weighted_ma']] 
+    load4_uncached = time()
     players=opt_data(players_2018_2020)
-    # st.table (players_2018_2020.head(3))
-    # st.table (players.sort_values(by='Cost', ascending=False).head())
+    
+    players.loc [ (players['Name']=='marcus_rashford'), 'Cost' ] = 1000
+    players.loc [ (players['Name']=='heung-min_son'), 'Cost' ] = 1000
+    players.loc [ (players['Name']=='jordan_henderson'), 'Cost' ] = 1000
+    players.loc [ (players['Name']=='jonjo_shelvey'), 'Cost' ] = 1000
+    players.loc [ (players['Name']=='marcos_alonso'), 'Cost' ] = 1000
 
+
+    load5_uncached = time()
     F_3_5_2=optimise_fpl(3,5,2, players)
     F_4_5_1=optimise_fpl(4,5,1, players)
     F_4_4_2=optimise_fpl(4,4,2, players)
@@ -72,16 +61,21 @@ def main():
     F_3_4_3=optimise_fpl(3,4,3, players)
     formations=[F_3_5_2,F_4_5_1,F_4_4_2,F_5_3_2,F_5_4_1,F_3_4_3]
     
-    col_df(players_2018_2020)
+    load6_uncached = time()
     players=table(formations)
+    load7_uncached = time()
     players=pd.merge(players, additional_info, on='Name', how='left')
+    load8_uncached = time()
 
-    cols_to_move = ['Name','Position','Count','team','EWM_Pts','Cost','F_3_5_2','F_4_5_1','F_4_4_2','F_5_3_2','F_5_4_1','F_3_4_3','Games_Total_Rolling',
-    'week','round','Games_Season_Rolling','Games_Total', 'Games_Season_Total']
+    cols_to_move = ['Name','Position','Count','team','EWM_Pts','PPG_Season_Rolling','Weighted_ma','Cost','Games_Season_to_Date','F_3_5_2','F_4_5_1','F_4_4_2','F_5_3_2','F_5_4_1','F_3_4_3','Games_Total_Rolling',
+    'week','round','Games_Total', 'Games_Season_Total','points_per_game']
     cols = cols_to_move + [col for col in players if col not in cols_to_move]
     players=players[cols]
-    
-    st.table(players)
+    format_dict = {'EWM_Pts':'{0:,.1f}','PPG_Season_Rolling':'{0:,.1f}','Weighted_ma':'{0:,.1f}'}
+
+    st.table(players.style.format(format_dict))
+    st.write(data.loc [ data['Name']=='jack_grealish']) 
+
     finish_uncached = time()
 
     benchmark_uncached = (
@@ -97,10 +91,11 @@ def main():
         )
     st.text(benchmark_uncached)
 
-def show_data(df, year, week, min_games_played):
-    return df [ (df['year']==year) & (df['week']==week) & (df['Games_Total_Rolling'] > min_games_played) ]
+def show_data(df, year, week, min_games_played, season_games_played):
+    # df=df.copy()
+    return df [ (df['year']==year) & (df['week']==week) & (df['Games_Total_Rolling'] >= min_games_played) & (df['Games_Season_to_Date'] >= season_games_played) ]
 
-
+#
 # @st.cache
 def combine_df(x,y,z):   # Actually turns out that its faster not to do the function here so not using it
     players_2018_2020=pd.concat([x,y,z], axis=0, sort=True) # this is where the timer function came in handy from pbaumgartner
@@ -128,14 +123,25 @@ def clean_format(df):
     df['Name']=df['Name'].apply(f)
     return df
 
+# https://stackoverflow.com/questions/40994756/pandas-rolling-and-ewm-to-completely-ignore-na-and-use-last-n-valid-data
+# https://stackoverflow.com/questions/9621362/how-do-i-compute-a-weighted-moving-average-using-pandas
+
 @st.cache
 def col_df(df):
     df['Game_1'] = np.where((df['minutes'] > 0.5), 1, 0)
     # df['Expo_Pts'] = df.groupby(['Name'])['week_points'].transform(lambda x: x.ewm(alpha=0.07).mean())
-    df['Clean_Pts'] = np.where(df['Game_1']==1,df['week_points'], np.NaN)
+    df['Clean_Pts'] = np.where(df['Game_1']==1,df['week_points'], np.NaN) # setting a slice on a slice - just suppresses warning....
     df = df.sort_values(by=['Name', 'year', 'week'], ascending=[True, True, True]) # THIS IS IMPORTANT!! EWM doesn't work right unless sorted
     df['EWM_Pts'] = df['Clean_Pts'].ewm(alpha=0.07).mean()
-    df['Games_Season_Rolling'] = df.groupby (['Name', 'year'])['Game_1'].cumsum()
+
+    weights = np.array([0.125, 0.25,0.5,1]) # the order mattered!! took me a while to figure this out
+    sum_weights = np.sum(weights)
+    df['Weighted_ma'] = (df['Clean_Pts'].rolling(window=4, center=False)\
+        .apply(lambda x: np.sum(weights*x) / sum_weights)) # raw=False
+        # using the fillna ensures no NaN as this function requires min 4 data points in a row - .fillna(method='ffill')
+        # so just be careful the result is the last time player had 4 weeks in a row
+
+    df['Games_Season_to_Date'] = df.groupby (['Name', 'year'])['Game_1'].cumsum()
     df['Games_Season_Total'] = df.groupby (['Name', 'year'])['Game_1'].transform('sum')
     df['Games_Total_Rolling'] = df.groupby (['Name'])['Game_1'].cumsum()
     df['Games_Total'] = df.groupby (['Name'])['Game_1'].transform('sum')
@@ -144,18 +150,18 @@ def col_df(df):
     df['Points_Total_Rolling'] = df.groupby (['Name'])['week_points'].cumsum()
     df['Points_Total'] = df.groupby (['Name'])['week_points'].transform('sum')
     df['PPG_Total'] = df['Points_Total'] / df['Games_Total']
-    df['PPG_Season_Rolling'] = df['Points_Season_Rolling'] / df['Games_Season_Rolling']
+    df['PPG_Season_Rolling'] = df['Points_Season_Rolling'] / df['Games_Season_to_Date']
     df['PPG_Total_Rolling'] = df['Points_Total_Rolling'] / df['Games_Total_Rolling']
     df['PPG_Season_Total'] = df['Points_Season_Total'] / df['Games_Season_Total']
     df["GK"] = (df["Position"] == 'GK').astype(float)
     df["DF"] = (df["Position"] == 'DF').astype(float)
     df["MD"] = (df["Position"] == 'MD').astype(float)
     df["FW"] = (df["Position"] == 'FW').astype(float)
-    df["LIV"] = (df["team"] == 'Liverpool').astype(float)
-    df["MC"] = (df["team"] == 'Man_City').astype(float)
     df['team'] = df['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Bournemouth', 4:'Brighton',5:'Burnley',6:'Chelsea',7:'Crystal_Palace',
     8:'Everton',9:'Leicester',10:'Liverpool',11:'Man_City',12:'Man_Utd',13:'Newcastle',14:'Norwich',15:'Sheffield_Utd',16:'Southampton',17:'Spurs',
     18:'Watford',19:'West_Ham',20:'Wolves'})
+    df["LIV"] = (df["team"] == 'Liverpool').astype(float)
+    df["MC"] = (df["team"] == 'Man_City').astype(float)
     df2=df.rename(columns = {'value':'Cost'})
 
     return df2
@@ -242,3 +248,5 @@ def combine_functions():
     return players_2018_2020
 
 main()
+
+# CHECK ROLLING TO SEE IF IT IGNORES NaN in calc
