fd6c931395cadd591cd8d1f765c08f0331404006
ZeNoonan <darragh.noonan@gmail.com>
2021-08-06 23:09:20 +0100

    update

--- FPL_Analysis.py
+++ FPL_Analysis.py
@@ -63,10 +63,10 @@ def main():
     st.sidebar.image(image, use_column_width=True)
 
     data_2021 = (data_2021_team_names( (prep_base_data(url2021, pick2021)).rename(columns = {'team_x':'team'}))).copy()
-    data_2020 = (data_2020_team_names( (prep_base_data(url2020, pick2020)).copy() )).copy()
-    data_2019 = (data_2019_team_names( (prep_base_data(url2019, pick2019)).copy() )).copy()
-    data_2018 = (data_2018_team_names( (prep_base_data(url2018, pick2018)).copy() )).copy()
-    data_2020 = (data_2020_clean_double_gw(data_2020)).copy()
+    # data_2020 = (data_2020_team_names( (prep_base_data(url2020, pick2020)).copy() )).copy()
+    # data_2019 = (data_2019_team_names( (prep_base_data(url2019, pick2019)).copy() )).copy()
+    # data_2018 = (data_2018_team_names( (prep_base_data(url2018, pick2018)).copy() )).copy()
+    # data_2020 = (data_2020_clean_double_gw(data_2020)).copy()
 
     # st.write('2021 check',data_2021)
 
@@ -75,9 +75,10 @@ def main():
         return pd.concat ([a,b], axis=0,sort = True)
 
     # all_seasons_df = (column_calcs( (combine_dataframes(data_2018,data_2019,data_2020,data_2021)).reset_index().copy() )).copy() # have added reset index duplicates in index?
-    all_seasons_df = (column_calcs( (combine_dataframes_2(data_2020,data_2021)).reset_index().copy() )).copy()
+    # all_seasons_df = (column_calcs( (combine_dataframes_2(data_2020,data_2021)).reset_index().copy() )).copy()
+    all_seasons_df = (column_calcs( data_2021)).reset_index().copy()
 
-    format_dict = {'EWM_Pts':'{0:,.1f}','PPG_Season_Total':'{0:,.1f} ppg','Weighted_ma':'{0:,.1f}','Weighted_mins':'{0:,.0f}','Points_Season_Total':'{0:,.0f}','last_2_years_Games_Total':'{0:,.0f}',
+    format_dict = {'EWM_Pts':'{0:,.1f}','PPG_Season_Total':'{0:,.1f} ppg','mins_ppg':'{0:,.0f}','sum_ppg':'{0:,.1f}','Weighted_ma':'{0:,.1f}','Weighted_mins':'{0:,.0f}','Points_Season_Total':'{0:,.0f}','last_2_years_Games_Total':'{0:,.0f}',
     'points_per_game':'{0:,.1f}','Price':'£{0:,.1f}m','PPG_Sn_Rmg':'{0:,.1f}','Gms_Ssn_Total':'{0:,.0f}','last_2_years_PPG':'{0:,.1f}','last_2_years_MPG':'{0:,.0f}',
     'ppg_last_10_games':'{0:,.1f}','Value':'{0:,.2f}','last_10_points_total':'{0:,.0f}','PPG_Sn_Rllg':'{0:,.1f}','Price':'{0:,.1f}','Price':'{0:,.1f}',
     'Pts_Sn_Rllg':'{0:,.0f}','Pts_Sn_Rllg_Rnk':'{0:,.0f}','Pts_Sn_Rllg_Rmg_Rnk':'{0:,.0f}','PPG_Sn_Rllg_Rmg_Rnk':'{0:,.0f}','PPG_Rllg_Rnk_Diff':'{0:,.0f}',
@@ -85,22 +86,26 @@ def main():
 
 
     player_data=column_calcs( ((data_2021)).reset_index().copy() )
-    player_data=player_data.loc[:,['full_name','week','year', 'minutes','Clean_Pts','Game_1','Weighted_ma','ppg_last_10_games','Weighted_mins','week_points','PPG_Sn_Rllg','Gms_Ssn_Total',
-    'Games_Total_Rolling', 'Games_Ssn_Rmg',
+    player_data=player_data.loc[:,['full_name','week','year', 'minutes','Clean_Pts','Game_1','week_points',
+    'last_8_games_calc','last_4_games_calc','last_2_games_calc','last_1_games_calc',
+    'last_8_points_calc','last_4_points_calc','last_2_points_calc','last_1_points_calc',
+    'last_15_games','last_14_games','last_12_games',
+    'last_15_points','last_14_points','last_12_points',
+    'sum_games','sum_points','sum_ppg','sum_mins','mins_ppg',
+    'PPG_Sn_Rllg','Gms_Ssn_Total','ppg_last_10_games',
+    'Games_Total_Rolling', 'Games_Ssn_Rmg','Weighted_ma','Weighted_mins',
     'Gms_Ssn_to_Date','PPG_Total',
     'points_per_game','Pts_Sn_Rllg_Rnk','Pts_Sn_Rllg_Rmg_Rnk','Pts_Rllg_Rnk_Diff','PPG_Sn_Rmg','PPG_Sn_Rllg_Rnk',
     'PPG_Sn_Rllg_Rmg_Rnk','PPG_Rllg_Rnk_Diff',
-    'last_8_games_calc','last_4_games_calc','last_2_games_calc','last_1_games_calc',
-    'last_8_points_calc','last_4_points_calc','last_2_points_calc','last_1_points_calc',
-    'sum_games','sum_points','sum_ppg',
+
     'last_10_games_total','last_10_points_total','Pts_Sn_Rllg','Pts_Sn_Rmg','last_2_years_PPG','last_2_years_MPG','last_2_years_Games_Total']]
     player_detail=player_data['full_name'].unique()
 
 
     # name_of_player = st.multiselect('Pick player for detail',player_detail)
-    willock=player_data.loc[player_data['full_name'].str.contains('willock')].tail(20)
+    willock=player_data.loc[player_data['full_name'].str.contains('barnes')]
     chilwell=player_data.loc[player_data['full_name'].str.contains('chilwell')].tail(20)
-    st.markdown(get_table_download_link(willock), unsafe_allow_html=True)
+    # st.markdown(get_table_download_link(willock), unsafe_allow_html=True)
     st.write(willock.style.format(format_dict))
     
     # st.write(player_data.loc[player_data['full_name'].str.contains('chilwell')].tail(15).style.format(format_dict))
@@ -115,7 +120,7 @@ def main():
     min_games_played = st.sidebar.number_input ("Minimum number of games ever", min_value=int(0),value=int(1))
     min_current_season_games_played = st.sidebar.number_input("Minimum number of games played from start of current Season",
     min_value=int(0),max_value=int(38), value=int(1))
-    last_2_years_games=st.sidebar.number_input ("Min last 2 years games ever", min_value=int(0),value=int(39))
+    last_2_years_games=st.sidebar.number_input ("Min last 2 years games ever", min_value=int(0),value=int(20))
 
     data=show_data(all_seasons_df, year, week, min_games_played, min_current_season_games_played,last_2_years_games)    
 
@@ -124,16 +129,16 @@ def main():
     player_names=data['full_name'].unique()
     names_selected = st.multiselect('Select which players you want excluded from lineup (e.g. due to injuries or suspension)',player_names)
     data_1=exclude_players(data,names_selected)
-    additional_info=data_1.loc[:,['full_name','week', 'Games_Total','Weighted_mins','ppg_last_10_games','Games_Total_Rolling', 'Gms_Ssn_Total','Games_Ssn_Rmg', 'Gms_Ssn_to_Date','PPG_Total',
+    additional_info=data_1.loc[:,['full_name','week', 'Games_Total','sum_ppg','mins_ppg','sum_mins','sum_games','ppg_last_10_games','Games_Total_Rolling', 'Gms_Ssn_Total','Games_Ssn_Rmg', 'Gms_Ssn_to_Date','PPG_Total',
     'points_per_game','Pts_Sn_Rllg_Rnk','Pts_Sn_Rllg_Rmg_Rnk','Pts_Rllg_Rnk_Diff','PPG_Sn_Rllg','PPG_Sn_Rmg','PPG_Sn_Rllg_Rnk','PPG_Sn_Rllg_Rmg_Rnk','PPG_Rllg_Rnk_Diff',
     'last_10_games_total','last_10_points_total','Pts_Sn_Rllg','Pts_Sn_Rmg','last_2_years_PPG','Weighted_ma','last_2_years_MPG','last_2_years_Games_Total']]
 
     
 
     st.sidebar.header("4. Optimise on which Points")
-    select_pts=st.sidebar.radio('Select the points you want to optimise',['Weighted_ma','PPG_Sn_Rllg','PPG_Sn_Rmg','Pts_Sn_Rmg','Pts_Sn_Rllg','Points_Season_Total', 'last_2_years_PPG','PPG_Total'])
+    select_pts=st.sidebar.radio('Select the points you want to optimise',['sum_ppg','Weighted_ma','PPG_Sn_Rllg','PPG_Sn_Rmg','Pts_Sn_Rmg','Pts_Sn_Rllg','Points_Season_Total', 'last_2_years_PPG','PPG_Total'])
     data_2=opt_data(data_1,select_pts)
-    # st.write('this is correct data 2 I hope', data_2)
+    # st.write('this is correct data 2 I hope', data_2.head(10))
 
     data_2022=pd.read_pickle('https://github.com/ZeNoonan/FPL/blob/master/raw_data_2022.pkl?raw=true')
     data_2022=data_2022.rename(columns={'Price':'Price_2022','team':'team_2022'})
@@ -159,7 +164,7 @@ def main():
     cols_to_move = ['full_name','team','Position','Price']
     cols = cols_to_move + [col for col in data_update if col not in cols_to_move]
     data_update=data_update[cols].reset_index().drop('index',axis=1)
-    # st.write('incorrect data check for NaN',data_update)
+    
     data_2=data_update
     # st.write(data_2.sort_values(by=select_pts,ascending=False).style.format({'last_2_years_PPG':'{0:,.1f}','PPG_Sn_Rllg':'{0:,.1f}','PPG_Season_Value':'{0:,.1f}',
     # 'Price':'£{0:,.1f}m'}))
@@ -178,7 +183,7 @@ def main():
 
     data_3=table(formations,select_pts)
     data_4=pd.merge(data_3, additional_info, on='full_name', how='left',suffixes=('', '_y'))
-    cols_to_move = ['full_name','Position','Count','team','Price','Weighted_ma','ppg_last_10_games','Weighted_mins','last_2_years_PPG','last_2_years_MPG','PPG_Sn_Rllg','Gms_Ssn_to_Date','last_2_years_Games_Total','Games_Total',
+    cols_to_move = ['full_name','Position','Count','team','Price','sum_ppg','mins_ppg','ppg_last_10_games','last_2_years_PPG','last_2_years_MPG','PPG_Sn_Rllg','Gms_Ssn_to_Date','last_2_years_Games_Total','Games_Total',
     'Pts_Sn_Rllg','PPG_Total','Pts_Sn_Rllg_Rnk',
     'F_3_4_3','F_4_3_3','F_3_5_2','F_4_5_1','F_4_4_2','F_5_3_2','F_5_4_1','F_5_2_3',
     'PPG_Sn_Rllg_Rnk','PPG_Sn_Rmg','PPG_Sn_Rllg_Rmg_Rnk','PPG_Rllg_Rnk_Diff','Games_Ssn_Rmg','Pts_Sn_Rmg','Pts_Sn_Rllg_Rmg_Rnk',
@@ -193,6 +198,8 @@ def main():
     st.write (data_5.set_index('full_name').style.format(format_dict))
 
     st.write (cost_total(data_5,selection1='Price', selection2=select_pts))
+
+    st.write('useful for sense checking',data_update.sort_values(by='Price',ascending=False))
 
 def get_image():
     response = requests.get("https://raw.githubusercontent.com/ZeNoonan/FPL/master/FPL_Image.jpg")
@@ -298,11 +305,25 @@ def column_calcs(df):
     df_calc['last_4_games_calc']=(df_calc['last_12_games']-df_calc['last_8_games_calc'])/2
     df_calc['last_2_games_calc']=(df_calc['last_14_games']-df_calc['last_12_games'])/4
     df_calc['last_1_games_calc']=(df_calc['last_15_games']-df_calc['last_14_games'])/8
+
+    df_calc['last_15_mins']=df_calc.groupby(['full_name'])['minutes'].rolling(window=15,min_periods=1, center=False).sum().reset_index(0,drop=True)
+    df_calc['last_14_mins']=df_calc.groupby(['full_name'])['minutes'].rolling(window=14,min_periods=1, center=False).sum().reset_index(0,drop=True)
+    df_calc['last_12_mins']=df_calc.groupby(['full_name'])['minutes'].rolling(window=12,min_periods=1, center=False).sum().reset_index(0,drop=True)
+    df_calc['last_8_mins_calc']=df_calc.groupby(['full_name'])['minutes'].rolling(window=8,min_periods=1, center=False).sum().reset_index(0,drop=True)
+    df_calc['last_4_mins_calc']=(df_calc['last_12_mins']-df_calc['last_8_mins_calc'])/2
+    df_calc['last_2_mins_calc']=(df_calc['last_14_mins']-df_calc['last_12_mins'])/4
+    df_calc['last_1_mins_calc']=(df_calc['last_15_mins']-df_calc['last_14_mins'])/8
+
+
     # st.write(df_calc.head(2))
     df_calc['sum_games']=df_calc.loc[:,['last_8_games_calc','last_4_games_calc','last_2_games_calc','last_1_games_calc']].sum(axis=1)
     df_calc['sum_points']=df_calc.loc[:,['last_8_points_calc','last_4_points_calc','last_2_points_calc','last_1_points_calc']].sum(axis=1)
-    df_calc['sum_ppg']=df_calc['sum_points']-df_calc['sum_games']
+    df_calc['sum_ppg']=df_calc['sum_points']/df_calc['sum_games']
+    df_calc['sum_mins']=df_calc.loc[:,['last_8_mins_calc','last_4_mins_calc','last_2_mins_calc','last_1_mins_calc']].sum(axis=1)
+    df_calc['mins_ppg']=df_calc['sum_mins']/df_calc['sum_games']
     df=pd.merge(df,df_calc,how='outer')
+    df['sum_ppg']=df['sum_ppg'].fillna(method='ffill')
+    df['mins_ppg']=df['mins_ppg'].fillna(method='ffill')
 
 
     df['Gms_Ssn_to_Date'] = df.groupby (['full_name', 'year'])['Game_1'].cumsum()
