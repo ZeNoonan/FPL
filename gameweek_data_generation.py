bbcc296c3b55b58ad898151da71def3672bc059a
ZeNoonan <darragh.noonan@gmail.com>
2021-10-24 17:07:57 +0100

    update

--- gameweek_data_generation.py
+++ gameweek_data_generation.py
@@ -4,12 +4,12 @@ import streamlit as st
 
 week_number = 38
 weeks = range(1, week_number+1)
-year = 2021
+year = 2020
 raw_data = []
 # @st.cache
 def run_function():
     for week in weeks:
-        path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2020-21/gws/gw%d.csv' % week
+        path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2019-20/gws/gw%d.csv' % week
         frame = pd.read_csv(path, encoding = "ISO-8859-1")
         frame ['week']= week
         frame ['year']= year
@@ -19,7 +19,8 @@ run_function()
 df1 = pd.concat(raw_data, ignore_index=True)
 df1=df1.rename(columns = {'element':'player_id'})
 st.table(df1.head())
-df1.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.pkl' % year)
+# df1.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.pkl' % year)
+df1.to_csv('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.csv' % year)
 
 # Gotta figure out cache, the concat is causing issues now, need to back to streamlit and see how it works 
 # 
