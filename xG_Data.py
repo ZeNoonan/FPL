40ebcd52c453e67ec9911ac3ce11feee6b34e7c9
ZeNoonan <darragh.noonan@gmail.com>
2022-03-18 09:00:03 +0000

    update

--- xG_Data.py
+++ xG_Data.py
@@ -27,6 +27,7 @@ def scraper_pl():
     'https://fbref.com/en/matches/f5b6f5c5/Crystal-Palace-Manchester-City-March-14-2022-Premier-League',
     'https://fbref.com/en/matches/59b3ee40/Brighton-and-Hove-Albion-Tottenham-Hotspur-March-16-2022-Premier-League',
     'https://fbref.com/en/matches/8ef5cc6b/Arsenal-Liverpool-March-16-2022-Premier-League',
+    'https://fbref.com/en/matches/029e5f94/Everton-Newcastle-United-March-17-2022-Premier-League',
     ]
 
     week=current_week
@@ -38,7 +39,7 @@ def scraper_pl():
         df.append(away_stats)
     dx=pd.concat(df)
     dx[('week','week')]=week
-    dx.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_29_test_test_test.pkl')
+    dx.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_%d.pkl' % week)
     return dx
 # scraper_pl()
 
@@ -57,6 +58,7 @@ def team_scraper_pl():
     'https://fbref.com/en/matches/f5b6f5c5/Crystal-Palace-Manchester-City-March-14-2022-Premier-League',
     'https://fbref.com/en/matches/59b3ee40/Brighton-and-Hove-Albion-Tottenham-Hotspur-March-16-2022-Premier-League',
     'https://fbref.com/en/matches/8ef5cc6b/Arsenal-Liverpool-March-16-2022-Premier-League',
+    'https://fbref.com/en/matches/029e5f94/Everton-Newcastle-United-March-17-2022-Premier-League',
     ]
 
     week=current_week
@@ -84,63 +86,14 @@ def team_scraper_pl():
         df.append(away_xg)
     dx=pd.concat(df)
     dx['week']=week
-    dx.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_29.pkl')
+    dx.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_%d.pkl' % week)
     return dx
 
 # team_scraper_pl()
 
-# check_test=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_test.pkl')
-# st.write('lets have a look', check_test)
 
-
-
-# urls_test='https://fbref.com/en/matches/17e86f90/Crystal-Palace-Tottenham-Hotspur-September-11-2021-Premier-League'
-# dfa=pd.read_html(urls_test)
-# # st.write('0',dfa[0])
-# # st.write('1',dfa[1]) 
-# st.write('2',dfa[2])
-# dfa[2].to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_names_test.pkl')
-# dfa[3].to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/home_stats_test.pkl')
-# dfa[10].to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/away_stats_test.pkl')
-# team_names_test=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_names_test.pkl')
-# away_stats_test=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/away_stats_test.pkl')
-# home_stats_test=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/home_stats_test.pkl')
-# st.write('team names', team_names_test)
-
-# team_names_test.columns=team_names_test.columns.swaplevel().droplevel()
-# st.write('home names', team_names_test.columns[0])
-# st.write('away names', team_names_test.columns[1])
-# home_stats_test.columns=home_stats_test.columns.droplevel()
-# away_stats_test.columns=away_stats_test.columns.droplevel()
-# st.write('home stats', home_stats_test)
-# home_stats_test=home_stats_test.iloc[-1:,:]
-# home_stats_test=home_stats_test.loc[:,['npxG','xA']].reset_index().drop('index',axis=1).rename(index={0:team_names_test.columns[0]})
-# away_stats_test=away_stats_test.iloc[-1:,:]
-# away_stats_test=away_stats_test.loc[:,['npxG','xA']].reset_index().drop('index',axis=1).rename(index={0:team_names_test.columns[1]})
-
-# home_conceded=(away_stats_test*-1).rename(index={team_names_test.columns[1]:team_names_test.columns[0]}).rename(columns={'npxG':'npxG_concede','xA':'xA_concede'})
-# st.write('conceded for home team',home_conceded)
-# home_stats_test=pd.concat([home_stats_test,home_conceded],axis=1)
-
-
-# st.write('home stats', home_stats_test)
-# st.write('away stats', away_stats_test)
-
-# st.write('4',dfa[4])
-# st.write('5',dfa[5])
-# st.write('6',dfa[6])
-# st.write('7',dfa[7])
-# st.write('8',dfa[8])
-# st.write('9',dfa[9])
-
-
-
-
-
-
-
-# week_23=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_23_stats.pkl')
-week_29=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_29_test_test_test.pkl')
+# week_30=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_30.pkl')
+# week_29=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_29.pkl')
 week_28=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_28_test_test_test.pkl')
 week_27=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_27_test_test_test.pkl')
 week_26=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_26_test_test_test.pkl')
@@ -170,16 +123,13 @@ week_3=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Dat
 week_2=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_2_test_test_test.pkl')
 week_1=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/week_1_test_test_test.pkl')
 
-# combined=pd.concat([week_22,week_21,week_20, week_19, week_18, week_17, week_16, week_15, week_14, week_13, week_12, week_11, week_10, week_9, week_8, week_7, week_6, week_5, week_4,week_3,week_2, week_1])
-
 combined=pd.concat([week_1,week_2,week_3,week_4,week_5,week_6,week_7,week_8,week_9,week_10,week_11,week_12,week_13,week_14,
-week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28,week_29])
+week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28])
+# week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28,week_29])
 combined[("Unnamed: 0_level_0","Player")]=combined[("Unnamed: 0_level_0","Player")].str.lower()
 combined[("Unnamed: 0_level_0","Player")]=combined[("Unnamed: 0_level_0","Player")].replace(" ", "_", regex=True)
 combined[("Expected","xg_xa")]=combined[("Expected","npxG")]+combined[("Expected","xA")]
-# combined.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/xg_2021_season_to_date.pkl')
 
-# st.write('combined', combined)
 data = combined.loc[:,[("week","week"),("Unnamed: 0_level_0","Player"),("Expected","npxG"),("Expected","xA"),("Expected","xG"),("Performance","Sh"),("Performance","SoT"),
 ("Unnamed: 3_level_0","Pos"),("Unnamed: 5_level_0","Min"),("Performance","Tkl"),("Performance","Int"),("Performance","Press"),
 ("Passes","Prog"),("Carries","Prog"),("Expected","xg_xa")]]
@@ -209,13 +159,6 @@ st.write('Just to note the Positions change on a week by week basis on FB Ref')
 updated_data=pd.merge(grouped_clean_data,position_data, on='Player', how='outer')
 # st.write(updated_data)
 
-# positions=pd.DataFrame(test_grouped_clean_data['Pos'].unique()).reset_index()
-
-# st.write(test_grouped_clean_data['Pos'].unique())
-
-# st.markdown(get_table_download_link(grouped_clean_data), unsafe_allow_html=True)
-
-
 # https://stackoverflow.com/questions/64145551/simplest-way-to-heatmap-selected-columns-in-python
 # https://seaborn.pydata.org/tutorial/color_palettes.html
 cm = sns.color_palette("Spectral", as_cmap=True)
@@ -238,25 +181,16 @@ st.write(midfielder_data.style.format(formatter={'xg_xa_avg': "{:,.2f}",'xg_xa_c
 st.write('Forwards:')
 st.write(forward_data.style.format(formatter={'xg_xa_avg': "{:,.2f}",'xg_xa_count': "{:,.0f}",'xg_xa_sum': "{:,.1f}",'shots_avg': "{:,.1f}",'std_dev': "{:,.1f}"}).background_gradient(cmap=cm, subset=["std_dev",'xg_xa_avg']))
 
-# st.write('cumulative figures',grouped_clean_data.sort_values(by='xg_xa_avg',ascending=False))
 
-# st.write(clean_data)
-# clean_data['xg_xa_count']=clean_data.groupby(['Player','week'])['Min'].transform('count')
-# st.write('check',clean_data)
 xg_xa_data=clean_data.loc[:,['Player','week','xg_xa']]
 xg_xa_data['average']=xg_xa_data.groupby('Player')['xg_xa'].transform(np.mean)
 xg_xa_data['count']=xg_xa_data.groupby('Player')['xg_xa'].transform('count')
 xg_xa_data=xg_xa_data[xg_xa_data['count']>= min_games_played]
-# st.write(xg_xa_data)
-
-
-# xg_xa_data['std_dev']=xg_xa_data.groupby('Player')['xg_xa'].transform(np.std)
-
-# xg_xa_data=grouped_clean_data.loc[:,['xg_xa_avg']].reset_index()
-# st.write(xg_xa_data.head())
+graph_data=xg_xa_data.groupby(['week','Player']).agg(xg_xa=('xg_xa','mean')).reset_index()
+graph_data['average']=graph_data.groupby('Player')['xg_xa'].transform(np.mean)
 
 with st.expander('xg_xa by week graph'):
-    chart_power= alt.Chart(xg_xa_data).mark_rect().encode(alt.X('week:O',axis=alt.Axis(title='week',labelAngle=0)),
+    chart_power= alt.Chart(graph_data).mark_rect().encode(alt.X('week:O',axis=alt.Axis(title='week',labelAngle=0)),
     alt.Y('Player',sort=alt.SortField(field='average', order='descending')),color=alt.Color('xg_xa:Q',scale=alt.Scale(scheme='redyellowgreen')))
     # https://altair-viz.github.io/gallery/layered_heatmap_text.html
     # https://vega.github.io/vega/docs/schemes/
@@ -264,7 +198,7 @@ with st.expander('xg_xa by week graph'):
     st.altair_chart(chart_power + text,use_container_width=True)
 
 with st.expander('Team Xg'):
-    week_29=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_29.pkl')
+    # week_29=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_29.pkl')
     week_28=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_28.pkl')
     week_27=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_27.pkl')
     week_26=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_26.pkl')
@@ -295,13 +229,16 @@ with st.expander('Team Xg'):
     week_1=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/1. Data/xG_Data/team_xg_week_1.pkl')
 
     combined_team_xg=pd.concat([week_1,week_2,week_3,week_4,week_5,week_6,week_7,week_8,week_9,week_10,week_11,week_12,
-    week_13,week_14,week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28,week_29])
+    week_13,week_14,week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28])
+    # week_13,week_14,week_15,week_16,week_17,week_18,week_19,week_20,week_21,week_22,week_23,week_24,week_25,week_26,week_27,week_28,week_29])
     # st.write(combined_team_xg)
     combined_team_xg['xg_xa']=combined_team_xg['npxG']+combined_team_xg['xA']
     combined_team_xg['xg_xa_concede']=combined_team_xg['npxG_concede']+combined_team_xg['xA_concede']
     
     combined_team_xg=combined_team_xg.reset_index().rename(columns={'index':'team'}).sort_values(by='week')
-    # combined_team_xg['average']=combined_team_xg.groupby('team')['xg_xa'].transform(np.mean)
+    # st.write('data', combined_team_xg)
+    combined_team_xg=combined_team_xg.groupby(['week','team']).agg(npxG=('npxG','mean'),npxG_concede=('npxG_concede','mean')).reset_index()
+    # st.write('data combine', test)
     combined_team_xg['average']=combined_team_xg.groupby('team')['npxG'].transform(np.mean)    
     # combined_team_xg['average_concede']=combined_team_xg.groupby('team')['xg_xa_concede'].transform(np.mean)
     combined_team_xg['average_concede']=combined_team_xg.groupby('team')['npxG_concede'].transform(np.mean)
@@ -322,31 +259,6 @@ with st.expander('Team Xg'):
     # https://vega.github.io/vega/docs/schemes/
     text=chart_power.mark_text().encode(text=alt.Text('npxG_concede:N',format=",.1f"),color=alt.value('black'))
     st.altair_chart(chart_power + text,use_container_width=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -705,67 +617,3 @@ with st.expander('Team Xg'):
 #     'https://fbref.com/en/matches/79b8fb6e/Leeds-United-Aston-Villa-March-10-2022-Premier-League',
 #     ]
 
-# week=1
-# for x in urls:
-#     dfa=pd.read_html(x)
-#     home_stats=dfa[3]
-#     away_stats=dfa[5]
-#     st.write('away 1',dfa[4])
-#     st.write('away 2',dfa[5])
-#     st.write('away 3',dfa[6]) 
-#     df.append(home_stats)
-#     df.append(away_stats)
-# dx=pd.concat(df)
-# st.write(dx)
-
-# dfa=pd.read_html('https://fbref.com/en/matches/c52500ad/Norwich-City-Liverpool-August-14-2021-Premier-League')
-# st.write(dfa[10])
-# st.write(dfa[11])
-# st.write(dfa[12])
-
-
-
-
-
-
-
-
-
-
-# data=pd.from
-
-# https://stackoverflow.com/questions/42462530/how-to-replace-the-white-space-in-a-string-in-a-pandas-dataframe
-# st.write ('below shows the highest xg_xa in the one game in the season')
-# st.table (combined.sort_values(by='xg_xa', ascending=False))
-
-
-
-
-
-
-# st.write (week_21['Min'].sum())
-# st.write ('number of estimated minutes', 11*90*20)
-
-# filt = combined['Week'].isin(['20','23','22','21'])
-# last_4 = combined[filt]
-# # st.table ( last_4.head() )
-# # st.write ('dataframe sum mins this is a check', last_4['Min'].sum(), 'my estimate', 11*90*20*4 )
-# xg_last4 = last_4.groupby('Player').agg(xg_xa_avg = ('xg_xa', 'mean'), xg_xa_sum = ('xg_xa', 'sum'), xg_xa_count=('xg_xa', 'count') ) 
-# st.write ('below is the average over the past 4 gameweeks')
-# st.table ( xg_last4.sort_values(by='xg_xa_avg', ascending=False).head(4) )
-
-# df=combined.copy().sort_values(by='Week', ascending=True)
-# df['moving_avg'] = df.groupby('Player')['xg_xa'].transform(lambda x: x.rolling(4, 4).mean())
-# df['moving_sum'] = df.groupby('Player')['xg_xa'].transform(lambda x: x.rolling(4, 4).sum())
-# df['moving_count'] = df.groupby('Player')['xg_xa'].transform(lambda x: x.rolling(4, 4).count())
-# df['cum_sum'] = df.groupby('Player')['xg_xa'].transform(lambda x: x.cumsum())
-# # Column for total goals and assists, then see the under or overperformance, but then regression to the mean
-# st.table ( df[df['Player']=='harry_maguire'] )
-# # st.write ('below shows the highest xg_xa cumulative for season')
-
-# # cols_to_move = ['Name','Position','Count','AvgPointsPerGame','Salary']
-# # cols = cols_to_move + [col for col in test2 if col not in cols_to_move]
-# # test2=test2[cols]
-# # st.table ( df.sort_values(by='cum_sum', ascending=False).head(10) )
-# df=df.sort_values(by='cum_sum', ascending=False)
-# df = df.drop_duplicates(subset=['Player'], keep = 'first')
