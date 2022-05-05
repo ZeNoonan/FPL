bbbdfd0c936e8602ea5f3e2cbaab7dd66751b50d
ZeNoonan <darragh.noonan@gmail.com>
2022-05-05 08:22:39 +0100

    update

--- gameweek_data_generation.py
+++ gameweek_data_generation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-week_number = 15
+week_number = 35
 weeks = range(1, week_number+1)
 year = 2022
 raw_data = []
@@ -10,6 +10,7 @@ raw_data = []
 def run_function():
     for week in weeks:
         path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/gw%d.csv' % week
+        # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2017-18/gws/gw%d.csv' % week
         frame = pd.read_csv(path, encoding = "ISO-8859-1")
         frame ['week']= week
         frame ['year']= year
@@ -20,6 +21,7 @@ df1 = pd.concat(raw_data, ignore_index=True)
 df1=df1.rename(columns = {'element':'player_id'})
 st.table(df1.head())
 # df1.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.pkl' % year)
+df1.to_csv('C:/Users/Darragh/Documents/Python/premier_league/raw_data_%d.csv' % year)
 df1.to_csv('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.csv' % year)
 
 # Gotta figure out cache, the concat is causing issues now, need to back to streamlit and see how it works 
