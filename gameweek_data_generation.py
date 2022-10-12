1b2391acc8ac926fd7266865335b6e508c2d1c9d
ZeNoonan <darragh.noonan@gmail.com>
2022-10-12 19:39:19 +0100

    update

--- gameweek_data_generation.py
+++ gameweek_data_generation.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-week_number = 37
+week_number = 10
 weeks = range(1, week_number+1)
-year = 2022
+year = 2023
 raw_data = []
 # @st.cache
 def run_function():
     for week in weeks:
-        path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/gw%d.csv' % week
+        # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/gw%d.csv' % week
         # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2017-18/gws/gw%d.csv' % week
+        path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/gws/gw%d.csv' % week
         frame = pd.read_csv(path, encoding = "ISO-8859-1")
         frame ['week']= week
         frame ['year']= year
@@ -20,9 +21,27 @@ run_function()
 df1 = pd.concat(raw_data, ignore_index=True)
 df1=df1.rename(columns = {'element':'player_id'})
 st.table(df1.head())
-# df1.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.pkl' % year)
 df1.to_csv('C:/Users/Darragh/Documents/Python/premier_league/raw_data_%d.csv' % year)
-df1.to_csv('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.csv' % year)
+
+raw_data_xp = []
+def run_function_xp():
+    for week in weeks:
+        # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/gw%d.csv' % week
+        # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2017-18/gws/gw%d.csv' % week
+        path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/gws/xP%d.csv' % week
+        frame = pd.read_csv(path, encoding = "ISO-8859-1")
+        frame ['week']= week
+        frame ['year']= year
+        raw_data_xp.append(frame)
+
+run_function_xp()
+df_xp = pd.concat(raw_data_xp, ignore_index=True)
+df_xp=df_xp.rename(columns = {'id':'player_id'})
+st.table(df_xp.head())
+df_xp.to_csv('C:/Users/Darragh/Documents/Python/premier_league/raw_data_xp_%d.csv' % year)
+
+
+# df1.to_csv('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/raw_data_%d.csv' % year)
 
 # Gotta figure out cache, the concat is causing issues now, need to back to streamlit and see how it works 
 # 
--- scraper_fpl.py
+++ scraper_fpl.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 
 data = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
 data = json.loads(data.content)
-# st.write(data)
+st.write(data)
 gameweeks = data['elements']
 # st.write(gameweeks)
 names=pd.DataFrame(data['elements']).apply(pd.Series)
@@ -30,14 +30,19 @@ cols_to_move = ['full_name','team','Position','Price']
 cols = cols_to_move + [col for col in url_csv if col not in cols_to_move]
 url_csv=url_csv[cols]
 
-url_csv['team'] = url_csv['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Brentford', 4:'Brighton',5:'Burnley',6:'Chelsea',7:'Crystal_Palace',
-8:'Everton',9:'Leicester',10:'Leeds_Utd',11:'Liverpool',12:'Man_City',13:'Man_Utd',14:'Newcastle',15:'Norwich',16:'Southampton',17:'Spurs',
-18:'Watford',19:'West_Ham',20:'Wolves'})
-url_csv['Price']=url_csv['Price']/10
+# url_csv['team'] = url_csv['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Brentford', 4:'Brighton',5:'Burnley',6:'Chelsea',7:'Crystal_Palace',
+# 8:'Everton',9:'Leicester',10:'Leeds_Utd',11:'Liverpool',12:'Man_City',13:'Man_Utd',14:'Newcastle',15:'Norwich',16:'Southampton',17:'Spurs',
+# 18:'Watford',19:'West_Ham',20:'Wolves'})
 
+url_csv['team'] = url_csv['team'].map({1: 'Arsenal', 2: 'Aston_Villa',3:'Bournemouth', 4:'Brentford', 5:'Brighton',6:'Chelsea',7:'Crystal_Palace',
+8:'Everton',9:'Fulham',10:'Leicester',11:'Leeds_Utd',12:'Liverpool',13:'Man_City',14:'Man_Utd',15:'Newcastle',
+16:'Nottingham_Forest',17:'Southampton',18:'Spurs',19:'West_Ham',20:'Wolves'})
+
+url_csv['Price']=url_csv['Price']/10
+# st.write(url_csv)
 filter_2022=url_csv.loc[:,['full_name','team','Position','Price','transfers_in_event','transfers_out_event']].copy()
 filter_2022['transfers_balance']=filter_2022['transfers_in_event']-filter_2022['transfers_out_event']
 # filter_2022.to_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/fpl_2022.pkl')
-filter_2022.to_csv('C:/Users/Darragh/Documents/Python/premier_league/week_transfers_in.csv')
+filter_2022.to_csv('C:/Users/Darragh/Documents/Python/premier_league/week_transfers_in_week_1.csv')
 # fpl_2022_data=pd.read_pickle('C:/Users/Darragh/Documents/Python/Fantasy_Football/fpl_1/fpl_2022.pkl')
-st.write(filter_2022.head())
+st.write(filter_2022)
