9b6c7d4e652c70348c82ef7220ed34d2591f2b45
ZeNoonan <darragh.noonan@gmail.com>
2023-03-07 20:21:18 +0000

    update

--- gameweek_data_generation.py
+++ gameweek_data_generation.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-week_number = 22
+st.set_page_config(layout="wide")
+
+week_number = 26
 weeks = range(1, week_number+1)
 # weeks=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 year = 2023
@@ -27,12 +29,14 @@ st.table(df1.head())
 df1.to_csv('C:/Users/Darragh/Documents/Python/premier_league/raw_data_%d.csv' % year)
 
 raw_data_xp = []
+weeks=[1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24]
 def run_function_xp():
     for week in weeks:
         # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/gw%d.csv' % week
         # path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2017-18/gws/gw%d.csv' % week
         path = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/gws/xP%d.csv' % week
         frame = pd.read_csv(path, encoding = "ISO-8859-1")
+        st.write('week passed XP',week)
         frame ['week']= week
         frame ['year']= year
         raw_data_xp.append(frame)
